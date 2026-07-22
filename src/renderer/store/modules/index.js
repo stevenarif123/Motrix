5fd854f98077ceb2dbf7a5e08c60422bca3e5faa
Steven Arif <stevenarif123@gmail.com>
2026-07-22 11:16:22 +0800

    fix: replace Webpack require.context with Vite import.meta.glob in store modules loader

--- src/renderer/store/modules/index.js
+++ src/renderer/store/modules/index.js
@@ -1,14 +1,15 @@
 /**
  * The file enables `@/store/index.js` to import all vuex modules
- * in a one-shot manner. There should not be any reason to edit this file.
+ * in a one-shot manner. Compatible with Vite import.meta.glob.
  */
 
-const files = require.context('.', false, /\.js$/)
+const files = import.meta.glob('./*.js', { eager: true })
 const modules = {}
 
-files.keys().forEach(key => {
-  if (key === './index.js') return
-  modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
-})
+for (const path in files) {
+  if (path === './index.js') continue
+  const moduleName = path.replace(/^\.\//, '').replace(/\.js$/, '')
+  modules[moduleName] = files[path].default || files[path]
+}
 
 export default modules
